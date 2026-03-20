b03c13a231a33f20d939d1e49414ec051e71a15a
hubligpt2-art <hubligpt2@gmail.com>
2026-03-20 14:58:43 +0530

    Add files via upload

---
+++ unboxdemo.java
@@ -0,0 +1,10 @@
+class unboxdemo
+{
+  public static void main(String a[])
+  {
+    Integer i=20;
+    int b=i;
+    System.out.println("primitive b is"=b);
+    System.out.println("wrapper class object i is"+i);
+  }
+}
